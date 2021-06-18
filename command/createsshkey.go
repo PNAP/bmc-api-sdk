72faa4fb1b79290a52b1d5aa439245392e1a6bb1
duskol <duskol@phoenixnap.com>
2021-06-18 18:11:17 +0200

    create sshkey command impl

--- command/createsshkey.go
+++ command/createsshkey.go
@@ -1,1 +1,35 @@
 package command
+
+import (
+	"net/http"
+
+	"github.com/PNAP/bmc-api-sdk/client"
+	"github.com/PNAP/bmc-api-sdk/dto"
+)
+
+// CreateSshKeyCommand represents command for sshkey creating. Use NewCreateSshKeyCommand to initilize command properly.
+type CreateSshKeyCommand struct {
+	requester client.Requester
+	sshKey    dto.SshKeyCreate
+}
+
+// Execute create new ssh key
+func (command *CreateSshKeyCommand) Execute() (*http.Response, error) {
+	var req = command.requester
+	return req.Post("ssh-keys", command.sshKey.ToBytes())
+}
+
+// SetRequester - sets requester to the command
+func (command *CreateSshKeyCommand) SetRequester(requester client.Requester) {
+	command.requester = requester
+}
+
+// SetSshKey - sets sshKey details to the command
+func (command *CreateSshKeyCommand) SetSshKey(sshKey dto.SshKeyCreate) {
+	command.sshKey = sshKey
+}
+
+//NewCreateSshKeyCommand - constructs new command of this type
+func NewCreateSshKeyCommand(requester client.Requester, sshKey dto.SshKeyCreate) *CreateSshKeyCommand {
+	return &CreateSshKeyCommand{requester, sshKey}
+}

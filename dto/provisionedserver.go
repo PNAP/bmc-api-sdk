1d7db5c510a510845c1a09aaf4dd5792340f1231
duskol <duskol@phoenixnap.com>
2021-06-21 13:27:13 +0200

    properties changed

--- dto/provisionedserver.go
+++ dto/provisionedserver.go
@@ -10,16 +10,19 @@ import (
 
 //ProvisionedServer represents server that should be provisioned (created) for the first time
 type ProvisionedServer struct {
-	ID          string `json:"id"`
-	Status      string `json:"status"`
-	Name        string `json:"hostname"`
-	Description string `json:"description"`
-
-	Os       string `json:"os"`
-	Type     string `json:"type"`
-	Location string `json:"location"`
-
-	SSHKeys []string `json:"sshKeys"`
+	Name                  string          `json:"hostname"`
+	Description           string          `json:"description"`
+	Os                    string          `json:"os"`
+	Type                  string          `json:"type"`
+	Location              string          `json:"location"`
+	Status                string          `json:"status"`
+	InstallDefaultSshKeys bool            `json:"installDefaultSshKeys"`
+	SshKeys               []string        `json:"sshKeys"`
+	SshKeyIds             []string        `json:"sshKeyIds"`
+	ReservationId         string          `json:"reservationId"`
+	PricingModel          string          `json:"pricingModel"`
+	NetworkType           string          `json:"networkType"`
+	Windows               OsConfiguration `json:"windows"`
 }
 
 //ToBytes performs conversion of struct to the io.Reader

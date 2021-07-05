4f93a7104fc83f07d159474f30acf7e6d86aa2d3
duskol <duskol@phoenixnap.com>
2021-07-05 15:31:04 +0200

    ToBytes func added

--- dto/sshkey.go
+++ dto/sshkey.go
@@ -1,8 +1,10 @@
 package dto
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -28,4 +30,14 @@ func (dto *SshKey) FromBytes(resp *http.Response) {
 		fmt.Println("API response body can not be converted to SshKey dto:", err)
 		os.Exit(1)
 	}
+}
+
+//ToBytes performs conversion of struct to the io.Reader
+func (dto SshKey) ToBytes() (reader io.Reader) {
+	requestByte, err := json.Marshal(dto)
+	if err != nil {
+		fmt.Println("SshKey details entered in invalid format:", err)
+		os.Exit(1)
+	}
+	return bytes.NewBuffer(requestByte)
 }
